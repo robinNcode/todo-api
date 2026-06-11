0065d6063134793b72c73899c0c004fed7cbda01
shahin.robin <shahin.robin@datasoft-bd.com>
2026-06-11 10:36:31 +0600

    feat: implement user and todo modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TodoModule, AuthModule, TodosModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
